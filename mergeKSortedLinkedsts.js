16830771b786fee617abcf6b5eb79bd6a7194dce
djaume <djaume@evernote.com>
2021-09-16 18:58:55 -0300

    fix(): Clean code - better approach

--- mergeKSortedLinkedsts.js
+++ mergeKSortedLinkedsts.js
@@ -10,46 +10,41 @@
  * @return {ListNode}
  */
 var mergeKLists = function (lists) {
-  const listsWithData = lists.filter((list) => list !== null);
-  if (listsWithData.length === 0) return null;
-  if (listsWithData.length === 1) return listsWithData[0];
-  let resultLinkedList = new ListNode();
-  let resultHead = resultLinkedList;
-
-  const mergeTwoLinkedLists = (l1, l2) => {
-    resultLinkedList = new ListNode();
-    resultHead = resultLinkedList;
+  if (lists.length === 0) {
+    let emptyNode = new ListNode();
+    emptyNode = null;
+    return emptyNode;
+  }
+  if (lists.length < 2) {
+    return lists[0];
+  }
+  const mergeTwoLists = (l1, l2) => {
+    let mergedList = new ListNode();
+    const head = mergedList;
     while (l1 && l2) {
-      if (l1.val > l2.val) {
-        resultLinkedList.next = new ListNode(l2.val);
-        resultLinkedList = resultLinkedList.next;
-        l2 = l2.next;
-      } else if (l1.val < l2.val) {
-        resultLinkedList.next = new ListNode(l1.val);
-        resultLinkedList = resultLinkedList.next;
+      if (l1.val <= l2.val) {
+        const node = new ListNode(l1.val);
+        mergedList.next = node;
         l1 = l1.next;
       } else {
-        resultLinkedList.next = new ListNode(l1.val);
-        resultLinkedList = resultLinkedList.next;
-        resultLinkedList.next = new ListNode(l2.val);
-        resultLinkedList = resultLinkedList.next;
-        l1 = l1.next;
+        const node = new ListNode(l2.val);
+        mergedList.next = node;
         l2 = l2.next;
       }
+      mergedList = mergedList.next;
     }
-    l1 && !l2 ? (resultLinkedList.next = l1) : (resultLinkedList.next = l2);
-    resultHead = resultHead.next;
+    if (l1 !== null) {
+      mergedList.next = l1;
+    } else if (l2 !== null) {
+      mergedList.next = l2;
+    }
+    return head.next;
   };
 
-  let listsToMerge = [];
-  for (let i = 0; i < listsWithData.length; i++) {
-    const list = listsWithData[i];
-    listsToMerge.push(list);
-    if (listsToMerge.length === 2) {
-      mergeTwoLinkedLists(listsToMerge[0], listsToMerge[1]);
-      listsToMerge = [resultHead];
-    }
+  let currentMergedList = mergeTwoLists(lists[0], lists[1]);
+  const offsetList = lists.splice(2);
+  for (let i = 0; i < offsetList.length; i++) {
+    currentMergedList = mergeTwoLists(currentMergedList, offsetList[i]);
   }
-
-  return resultHead;
+  return currentMergedList;
 };
--- mergeTwoSortedLinkedList.js
+++ mergeTwoSortedLinkedList.js
@@ -5,41 +5,38 @@
  *     this.next = (next===undefined ? null : next)
  * }
  */
-
-/**   L1 => 2-->4
- *    L2 =>1-->3-->4
+/**
  * @param {ListNode} l1
  * @param {ListNode} l2
  * @return {ListNode}
  */
+
 var mergeTwoLists = function (l1, l2) {
+  if (l1 == null && l2 == null) {
+    let empty = new ListNode();
+    empty = null;
+    return empty;
+  }
+  if (l1 && !l2) {
+    return l1;
+  } else if (!l1 && l2) {
+    return l2;
+  }
   let mergedList = new ListNode();
-  const head = mergedList;
+  const headToMergedList = mergedList;
   while (l1 && l2) {
-    if (l1.val > l2.val) {
-      const newNode = new ListNode(l2.val);
-      mergedList.next = newNode;
-      mergedList = mergedList.next;
-      l2 = l2.next; //remove first node
-    } else if (l1.val < l2.val) {
-      const newNode = new ListNode(l1.val);
-      mergedList.next = newNode;
+    const l1NodeVal = l1.val;
+    const l2NodeVal = l2.val;
+    if (l1NodeVal <= l2NodeVal) {
+      mergedList.next = new ListNode(l1NodeVal);
       mergedList = mergedList.next;
       l1 = l1.next;
     } else {
-      mergedList.next = new ListNode(l1.val);
+      mergedList.next = new ListNode(l2NodeVal);
       mergedList = mergedList.next;
-      mergedList.next = new ListNode(l2.val);
-      mergedList = mergedList.next;
-      l1 = l1.next;
       l2 = l2.next;
     }
   }
-
-  if (l1 && !l2) {
-    mergedList.next = l1;
-  } else if (!l1 && l2) {
-    mergedList.next = l2;
-  }
-  return head.next;
+  l1 !== null ? (mergedList.next = l1) : (mergedList.next = l2);
+  return headToMergedList.next;
 };
