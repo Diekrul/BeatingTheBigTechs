36d673a85556482573969d9184a7bc0a811e39b4
djaume <djaume@evernote.com>
2021-09-18 12:33:40 -0300

    Intervals - Non Overlaping Intervals

--- nonOverlapingIntervals.js
+++ nonOverlapingIntervals.js
@@ -1,27 +1,17 @@
-/**
- * @param {number[][]} intervals
- * @return {number}
- */
 var eraseOverlapIntervals = function (intervals) {
-  for (let i = 0; i < intervals.length; i++) {
-    intervals.sort((a, b) => a[0] - b[0]);
-  }
-  const minStartingInterval = intervals[0][0];
-  for (let i = 0; i < intervals.length; i++) {
-    intervals.sort((a, b) => a[1] - b[1]);
-  }
-  let lastFinished = minStartingInterval;
-  const result = [];
-  let intervalsRemoved = 0;
-  for (let i = 0; i < intervals.length; i++) {
-    let start = intervals[i][0];
-    let end = intervals[i][1];
-    if (start >= lastFinished) {
-      result.push(intervals[i]);
-      lastFinished = end;
+  intervals.sort((a, b) => a[1] - b[1]);
+  let intervalsToRemove = 0;
+  let currentIntervalThatFinishLatest = intervals[0][1];
+
+  for (let i = 0; i < intervals.length - 1; i++) {
+    const nextStart = intervals[i + 1][0];
+    const nextEnd = intervals[i + 1][1];
+    if (currentIntervalThatFinishLatest > nextStart) {
+      intervalsToRemove++;
     } else {
-      intervalsRemoved++;
+      currentIntervalThatFinishLatest = nextEnd;
     }
   }
-  return intervalsRemoved;
+
+  return intervalsToRemove;
 };
