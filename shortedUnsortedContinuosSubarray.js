51dc9475081f240042024dbaf8b515a9fa96db19
diego jaume <jaume_style2@hotmail.com>
2022-02-19 15:35:14 -0300

    Array - Improve(): Shortest Unsorted Continuous Subarray

--- shortedUnsortedContinuosSubarray.js
+++ shortedUnsortedContinuosSubarray.js
@@ -3,22 +3,17 @@
  * @return {number}
  */
 var findUnsortedSubarray = function (nums) {
-  const sorted = [...nums].sort((a, b) => a - b);
   let start = null;
   let end = null;
-
-  for (let i = 0; i < sorted.length; i++) {
-    if (sorted[i] !== nums[i]) {
+  const sortedNums = [...nums].sort((a, b) => a - b);
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== sortedNums[i]) {
       if (start === null) {
         start = i;
+      } else {
         end = i + 1;
-      } else if (start !== null && sorted[i] !== nums[i]) {
-        end = i;
       }
     }
   }
-  if (!start && !end) {
-    return 0;
-  }
-  return end - start + 1;
+  return end - start;
 };
