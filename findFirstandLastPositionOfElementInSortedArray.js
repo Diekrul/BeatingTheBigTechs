84455987f09a98538320400b2864a7673a275af9
Diego Jose Jaume Guazzini <diegojosej@microsoft.com>
2022-02-28 14:47:29 -0300

    Array - findFirstAndLastPositionOfElementInSortedArray

--- findFirstandLastPositionOfElementInSortedArray.js
+++ findFirstandLastPositionOfElementInSortedArray.js
@@ -1,50 +1,49 @@
-/*
-Given an array of integers nums sorted in ascending order, find the starting and ending position of a given target value.
-
-If target is not found in the array, return [-1, -1].
-
-You must write an algorithm with O(log n) runtime complexity.
-
-
-Input: nums = [5,7,7,8,8,10], target = 8
-Output: [3,4]
-*/
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
 var searchRange = function (nums, target) {
-  //binary search + buscar en ventana.
-  let foundedAtPosition = -1;
-  let firstPosition = -1;
-  let lastPosition = -1;
-  const binarySearch = (i, j) => {
-    const middle = Math.floor((i + j) / 2);
-    if (i > j) {
+  let result;
+  let elementPosition = null;
+
+  const binarySearch = (start, end) => {
+    const middle = Math.floor((start + end) / 2);
+    if (start > end) {
+      result = [-1, -1];
       return;
     }
-    if (nums[middle] === target) {
-      foundedAtPosition = middle;
-    } else if (nums[middle] > target) {
-      binarySearch(i, middle - 1);
+    if (nums[middle] > target) {
+      binarySearch(start, middle - 1);
     } else if (nums[middle] < target) {
-      binarySearch(middle + 1, j);
+      binarySearch(middle + 1, end);
+    } else {
+      elementPosition = middle;
+      return;
     }
   };
 
-  const findRange = () => {
-    let temp = foundedAtPosition;
-    while (target === nums[temp]) {
-      temp--;
+  const findRange = (elementPosition) => {
+    let startRange, endRange;
+    for (let i = elementPosition; i >= 0; i--) {
+      if (nums[i] === target) {
+        startRange = i;
+      } else {
+        break;
+      }
     }
-    temp++;
-    firstPosition = temp;
-    temp = foundedAtPosition;
-    while (target === nums[temp]) {
-      temp++;
+    for (let i = elementPosition; i < nums.length; i++) {
+      if (nums[i] === target) {
+        endRange = i;
+      } else {
+        break;
+      }
     }
-    temp--;
-    lastPosition = temp;
+    result = [startRange, endRange];
   };
 
   binarySearch(0, nums.length - 1);
-  if (foundedAtPosition === -1) return [-1, -1];
-  findRange();
-  return [firstPosition, lastPosition];
+  if (result) return result;
+  findRange(elementPosition);
+  return result;
 };
