a28fed1f0d8792ba48f13cd7ec4398deb28bee39
Diego Jose Jaume Guazzini <diegojosej@microsoft.com>
2022-03-01 13:15:56 -0300

    Binary - numberOf1Bits

--- numberOf1Bits.js
+++ numberOf1Bits.js
@@ -6,7 +6,9 @@ var hammingWeight = function (n) {
   let count = 0;
   while (n !== 0) {
     count = count + (n & 1);
+    // if n & 1 === 0, means that n was 0 because any number that is not 1, AND 1, returns 0
     n = n >>> 1;
+    // Push the digits from left to right. exmaple: 110=>011=>001=>000
   }
   return count;
 };
